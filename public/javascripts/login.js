febd839c18cdc0778c4f4236b16a356c5f57a596
gaozhu <gaodazhu@126.com>
2013-03-24 17:58:46 +0800

    gaozhu
    第一个版本

---
+++ public/javascripts/login.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: gaozhu
+ * Date: 13-3-24
+ * Time: 下午2:53
+ * To change this template use File | Settings | File Templates.
+ */
+$(function(){
+    $.messager.alert("sdfs","dsafas","warning");
+})
